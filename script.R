17a0796bdc3af33525fdb90565246f524ea0a346
javiergf14 <antoniojavier.gonzalez.ferrer@alumnos.upm.es>
2017-01-03 21:52:15 +0100

    Some tries with logistic regression.

--- script.R
+++ script.R
@@ -13,3 +13,13 @@ colnames(df) <- c("fixed_acidity", "volatile_acidity", "citic_acid", "residual_s
 # Quality and type are categorical variables.
 df$quality <- as.factor(df$quality)
 df$type <- as.factor(df$type)
+
+# Logistic regression...
+log.F=glm(type~fixed_acidity+volatile_acidity+citic_acid+residual_sugar+chlorides+free_sulfur_dioxide+total_sulfur_dioxide+density+ 
+            pH+sulphates+alcohol, data=df, family=binomial)
+
+
+log.B=glm(type~1,data=df,family=binomial)
+Step.for=step(log.B, scope=list(upper=log.F),direction="forward")
+
+log.m=glm(type~total_sulfur_dioxide+sulphates,data=df,family=binomial)
