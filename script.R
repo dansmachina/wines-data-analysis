64a90600a0cb96c7a20c9fb39728725d1237901d
javiergf14 <antoniojavier.gonzalez.ferrer@alumnos.upm.es>
2017-01-03 21:47:09 +0100

    Names of the variables and put in factor the categorical variables.

--- script.R
+++ script.R
@@ -3,3 +3,13 @@
 # Javier
 setwd("/mnt/sda1/Dropbox/EIT/Intelligent Data Analysis/elective/wines-data-analysis")
 
+df <- read.table("data10.txt",  header = FALSE)
+str(df)
+
+colnames(df) <- c("fixed_acidity", "volatile_acidity", "citic_acid", "residual_sugar", 
+                  "chlorides", "free_sulfur_dioxide", "total_sulfur_dioxide", "density", 
+                  "pH", "sulphates", "alcohol", "quality", "type")
+
+# Quality and type are categorical variables.
+df$quality <- as.factor(df$quality)
+df$type <- as.factor(df$type)
