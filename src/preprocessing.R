b344319b0ef05bc06a7e25719c9b0be85d2f1b40
javiergf14 <antoniojavier.gonzalez.ferrer@alumnos.upm.es>
2017-01-04 13:45:06 +0100

    Added preprocessing script. It modifies the raw datasets to convert it to the processed dataset.

---
+++ src/preprocessing.R
@@ -0,0 +1,26 @@
+library(readr)
+
+# Setting working directory
+# Javier
+setwd("/mnt/sda1/Dropbox/EIT/ida-Intelligent_Data_Analysis/elective/wines-data-analysis/src")
+
+white <- read_delim("../data/raw/winequality-white.csv",";", escape_double = FALSE, trim_ws = TRUE)
+white$type = 0
+
+red <- read_delim("../data/raw/winequality-red.csv",";", escape_double = FALSE, trim_ws = TRUE)
+red$type = 1
+
+df <- rbind(white, red)
+colnames(df) <- c("fixed_acidity", "volatile_acidity", "citic_acid", "residual_sugar", 
+                     "chlorides", "free_sulfur_dioxide", "total_sulfur_dioxide", "density", 
+                     "pH", "sulphates", "alcohol", "quality", "type")
+
+# Remove missing values
+df <- df[complete.cases(df), ]
+# Quality and type are categorical variables.
+df$quality <- as.factor(df$quality)
+df$type <- as.factor(df$type)
+
+# Write dataset into csv file.
+write.table(df, file="../data/processed/wines.csv")
+
